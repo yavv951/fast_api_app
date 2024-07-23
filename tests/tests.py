2c8079b6606ab31daf37e0b0508f76e5b4dd2ccd
Vadim <user.email>
2024-07-23 20:34:02 +0300

    --

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,19 @@
 import allure
 
 
-@allure.feature("Users")
+@allure.feature("Get user")
 def test_get_user():
+    pass
+
+
+@allure.feature("Create user")
+def test_create_user():
+    pass
+
+@allure.feature("Delete user")
+def test_delete_user():
+    pass
+
+@allure.feature("Update user")
+def test_update_user():
     pass
